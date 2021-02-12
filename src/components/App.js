3ed01d6cd979e4d79e208f276d14a764a034e3fa
Monyneath Mol <monyneathmol@Monyneath-2.local>
2021-02-12 23:57:36 +0700

    start added client app

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,59 @@
 import React, { Component } from 'react';
 import logo from '../logo.png';
 import './App.css';
-
+import Web3 from 'web3';
+import Marketplace from '../abis/Marketplace';
+import NavBar from "./NavBar";
 class App extends Component {
+
+
+  constructor(prop) {
+    super(prop)
+    this.state = {
+      account : '',
+      productCount : 0,
+      products : [],
+      isloading : true
+    }
+  }
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockChainData()
+  }
+
+  async loadBlockChainData() {
+    const web3 = window.web3
+    const account = await web3.eth.getAccounts();
+    this.setState({ account : account[0 ]})
+    console.log(account)
+    const networkID = await  web3.eth.net.getId();
+    const networkData = Marketplace.networks[networkID]
+    if (networkData) {
+      const marketplace = web3.eth.Contract(Marketplace.abi,networkData.address)
+      console.log(marketplace)
+    }else{
+      window.alert('Contract has\'t deploy yet')
+    }
+
+  }
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
+        <NavBar account={this.state.account}/>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
@@ -28,6 +66,7 @@ class App extends Component {
                   <img src={logo} className="App-logo" alt="logo" />
                 </a>
                 <h1>Dapp University Starter Kit</h1>
+                <p>Account1 : {this.state.account}</p>
                 <p>
                   Edit <code>src/components/App.js</code> and save to reload.
                 </p>
